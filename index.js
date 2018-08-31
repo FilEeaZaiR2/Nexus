33268d2f6a92c69026b75dc6c5cb2956fc2e0072
FilEeaZaiR2 <42357570+FilEeaZaiR2@users.noreply.github.com>
2018-08-31 15:16:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+//Base bot discord :
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+var prefix = "x.";
+
+//Login + connexion du bot :
+client.login("NDgzMzczMTY2ODM2ODQyNTA2.DmShNA.BVDSwaRkMR79D60mCYcpnybJBa8");
+
+client.on("ready", () => {
+    console.log(`Connexion en cours ... Voici mon prefix : "` + prefix + `"`);
+});
+
+client.on("guildMemberAdd", member => {
+    const bvn = member.guild.channels.find(m => m.name === "discussion-générale");
+if(!bvn) return;
+//var role = member.guild.roles.find("name", "Members");
+let regles = member.guild.channels.find("name", "règlement");
+bvn.send(`Bienvenue ${member}, n'hésite pas à lire le ` + regles + ` pour plus d'informations !`)
+})
+
+client.on("guildMemberAdd", member => {
+    var members = member.guild.roles.find("name", "Members");
+
+    if(!members) return;
+
+    member.addRole(members)
+})
