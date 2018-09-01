f463f2afd95efb5c35cd9d0da07ae10cabf7de49
FilEeaZaiR2 <42357570+FilEeaZaiR2@users.noreply.github.com>
2018-09-01 14:57:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -27,3 +27,19 @@ client.on("guildMemberAdd", member => {
 
     member.addRole(members)
 })
+
+if(message.content === prefix + "membercount" && message.channel.type != "dm"){
+
+  var mbr_embed = new Discord.RichEmbed()
+  .setTitle("Membres")
+  .setDescription(`Sur le serveur **${message.guild.name}**, il y'a :\n **${message.guild.members.filter(o => o.presence.status === 'online').size}** En Ligne\n**${message.guild.members.filter(i => i.presence.status === 'idle').size}** Ne pas déranger\n**${message.guild.members.filter(off => off.presence.status === 'offline').size}** Hors ligne/invisible\n**${message.guild.members.filter(s => s.presence.status === 'streaming').size}** En Streameur`)
+  .addField(`Il y'a : **${message.guild.members.filter(member => !member.user.bot).size}** humains`, "** **")
+	.addField(`Il y'a : **${message.guild.members.filter(member => member.user.bot).size}** bots`, "** **" )
+	.addField(`Le propriétaire est : ${message.guild.owner.user.tag}`, `** **`)
+  message.channel.send(mbr_embed)
+}
+
+
+if(message.content === prefix + "channelcount" && message.channel.type != "dm"){
+  message.channel.send(`Sur le serveur **${message.guild.name}**, il y'a **${message.guild.channels.size}** channels vocaux/écrits !`)
+}
